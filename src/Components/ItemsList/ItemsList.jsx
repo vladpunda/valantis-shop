b14b7594af1cc651e182851252ef6ec8b765ca3a
vladpunda <vladalv01@mai.ru>
2024-03-06 18:06:58 +0300

    add

--- src/Components/ItemsList/ItemsList.jsx
+++ src/Components/ItemsList/ItemsList.jsx
@@ -54,6 +54,7 @@ export default function ItemList() {
 
   useEffect(() => {
     getIds(offsetNumber);
+    // eslint-disable-next-line
   }, [offsetNumber]);
 
   const handlePageClick = (limNumber) => {

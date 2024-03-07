4e86a2e7f3f077bd2add6ba84f49c413aa268d2d
vladpunda <vladalv01@mai.ru>
2024-03-07 10:23:37 +0300

    add

--- src/API/index.js
+++ src/API/index.js
@@ -6,7 +6,8 @@ const timeStamp = format(new Date(), "yyyyMMdd");
 const md5Hash = md5(`Valantis_${timeStamp}`);
 
 export const httpClient = axios.create({
-  baseURL: "http://api.valantis.store:40000/",
+  baseURL:
+    "https://cors-anywhere.herokuapp.com/http://api.valantis.store:40000/",
   timeout: 600000,
-  headers: { "X-Auth": md5Hash, "Access-Control-Allow-Origin": "*" },
+  headers: { "X-Auth": md5Hash },
 });
